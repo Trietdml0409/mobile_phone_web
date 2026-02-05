ddaa7b607261cc35321cf333929cb83d64868c4d
Aidan <hxtruong6@gmail.com>
2026-02-05 15:52:24 +0700

    Enhance cart functionality by adding product management features. Introduced methods for adding, removing, and updating products in the cart, along with a new CartProductState type for better state management. Updated CartContext and CartProvider to support these changes.

--- src/shared/context/cartContext.ts
+++ src/shared/context/cartContext.ts
@@ -1,10 +1,15 @@
 import { createContext } from "react";
+import { CartProductState, IProduct } from "../types/common.types";
 
 interface CartContextType {
   productIds: number[];
   addProductId: (productId: number) => void;
   removeProductId: (productId: number) => void;
   clearCart: () => void;
+  cartProducts: CartProductState;
+  addProductToCart: (product: IProduct, quantity: number) => void;
+  removeProductFromCart: (productId: number) => void;
+  removeProductByeOneFromCart: (productId: number) => void;
 }
 
 export const CartContext = createContext<CartContextType>({
@@ -12,6 +17,10 @@ export const CartContext = createContext<CartContextType>({
   addProductId: () => {},
   removeProductId: () => {},
   clearCart: () => {},
+  cartProducts: {},
+  addProductToCart: () => {},
+  removeProductFromCart: () => {},
+  removeProductByeOneFromCart: () => {},
 });
 
 
--- src/shared/providers/CartProvider.tsx
+++ src/shared/providers/CartProvider.tsx
@@ -1,58 +1,159 @@
 import { useState, useEffect } from "react";
 import { CartContext } from "../context/cartContext";
+import { CartProductState, IProduct } from "../types/common.types";
 
 export function CartProvider({ children }: { children: React.ReactNode }) {
-  const [productIds, setProductIds] = useState<number[]>([])
+  const [productIds, setProductIds] = useState<number[]>([]);
+  const [cartProducts, setCartProducts] = useState<CartProductState>({});
 
-  useEffect(()=>{
-		//Get the data in JSON form by the ID using getItems
-    const saveCart:string|null = localStorage.getItem("cartProductIds")
+  /*
+  {
+    productId: number;
+    quantity: number;
+    price:
+    addToCartAt: //
+  }[] : list of products in the cart
+
+  product: { id:123, name: "ComputuerI7", price: 150}
+
+  => [
+    { productId: 123, quantity: 4, price: 150x4, addToCartAt: "2026-02-05 10:00:00" },
+  ]
+
+  => O(n) time complexity
+  => disadvantage: unique productId
+
+
+  --------------------
+
+  {
+     [productId]: {
+       quantity: number;
+       price: number;
+       addToCartAt: string;
+     }
+  } : dictionary of products in the cart
+
+  => {
+    "123": {
+      quantity: 4,
+      price: 150x4,
+      addToCartAt: "2026-02-05 10:00:00"
+    }
+  }
+
+  => O(1) time complexity
+
+  */
+
+  useEffect(() => {
+    //Get the data in JSON form by the ID using getItems
+    const saveCart: string | null = localStorage.getItem("cartProductIds");
     //2 important cases, if the data is found or if it is not found
-    if(saveCart){
-      try{
-	      //transform the data into list of object
+    if (saveCart) {
+      try {
+        //transform the data into list of object
         const values = JSON.parse(saveCart) as number[];
 
         //set data
         // eslint-disable-next-line react-hooks/set-state-in-effect
         setProductIds(values);
-      }catch(error){
-         console.error("Error parsing saved liked product ids: ", error);
+      } catch (error) {
+        console.error("Error parsing saved liked product ids: ", error);
       }
     }
-  },[])
+  }, []);
 
   //When the productIds change, the local storage update the value
-  useEffect(()=>{
-    localStorage.setItem("cartProductIds",JSON.stringify(productIds))},[productIds])
+  useEffect(() => {
+    localStorage.setItem("cartProductIds", JSON.stringify(productIds));
+  }, [productIds]);
   //Dependency is productIds
 
-  const addProductId = (productId: number, numberOfProduct: number) => {
+  const addProductId = (productId: number) => {
     // setProductIds((prev) => [...prev, productId]);
     const newProductIds = [...productIds, productId];
     setProductIds(newProductIds);
   };
 
-
   const removeProductId = (productId: number) => {
     setProductIds((prev: number[]) =>
-      prev.filter((id: number) => id !== productId)
+      prev.filter((id: number) => id !== productId),
     );
-
   };
 
+  const addProductToCart = (product: IProduct, quantity: number = 1) => {
+    const productKey = product.id.toString();
+    console.log("xxxx productKey", productKey, product);
+
+    const newCartProducts: CartProductState = { ...cartProducts };
+
+    // product already in cart
+    if (cartProducts[productKey]) {
+      newCartProducts[productKey].quantity += quantity;
+      newCartProducts[productKey].totalPrice =
+        product.price * newCartProducts[productKey].quantity;
+    } else {
+      // new product in cart
+      newCartProducts[productKey] = {
+        productId: product.id,
+        quantity: quantity,
+        price: product.price,
+        totalPrice: product.price * quantity,
+        addedAt: new Date().toISOString(),
+      };
+    }
+
+    console.log("xxxx addProductToCart", newCartProducts);
+
+    setCartProducts({ ...newCartProducts });
+  };
+
+  const removeProductFromCart = (productId: number) => {
+    const productKey = productId.toString();
+    const newCartProducts: CartProductState = { ...cartProducts };
+    delete newCartProducts[productKey]; // delete the product from the cart
+    setCartProducts({ ...newCartProducts });
+  };
+
+  const removeProductByeOneFromCart = (productId: number) => {
+    const productKey = productId.toString();
+
+    // logic:
+    // 1. check if product in cart
+    // 2. Yes => decrease quantity by 1 + update total price
+    // 3. No => remove product from cart
+
+    const newCartProducts: CartProductState = { ...cartProducts };
+
+    if (cartProducts[productKey]) {
+      newCartProducts[productKey].quantity -= 1;
+      newCartProducts[productKey].totalPrice =
+        newCartProducts[productKey].totalPrice -
+        newCartProducts[productKey].price;
+
+      setCartProducts({ ...newCartProducts });
+    }
+  };
 
   const clearCart = () => {
     setProductIds([]);
   };
   return (
     <CartContext.Provider
-      value={{ productIds, addProductId, removeProductId, clearCart }}
+      value={{
+        productIds,
+        addProductId,
+        removeProductId,
+        clearCart,
+
+        cartProducts,
+        addProductToCart,
+        removeProductFromCart,
+        removeProductByeOneFromCart,
+      }}
     >
       {children}
     </CartContext.Provider>
   );
 }
-
-
-
